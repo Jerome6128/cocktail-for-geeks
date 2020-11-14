2a68d1d1d483630315350dcbc552d0112ec68c8b
Jerome Gratelle <jerome.gratelle@air-indemnite.com>
2020-11-14 08:05:42 +0100

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,49 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'json'
+require 'open-uri'
+require 'faker'
+
+puts 'Cleaning database...'
+Cocktail.destroy_all
+Ingredient.destroy_all
+
+puts 'Ingredients seeding'
+puts 'Creating ingredients..."-'
+
+Ingredient.create(name: "Ice")
+Ingredient.create(name: "Mint leaves")
+url = 'https://www.thecocktaildb.com/api/json/v1/1/list.php?i=list'
+ingredients_serialized = open(url).read
+ingredients = JSON.parse(ingredients_serialized)['drinks']
+ingredients.each do |ingredient|
+  Ingredient.create(name: ingredient['strIngredient1'])
+end
+
+puts 'Cocktails seeding'
+puts 'Creating cocktails..."-'
+100.times do
+  file = URI.open('https://giantbomb1.cbsistatic.com/uploads/original/9/99864/2419866-nes_console_set.png')
+  cocktail = Cocktail.create(name: Faker::ProgrammingLanguage.unique.name)
+  #cocktail.url = "ajldwzjt25gm8mts0jbz"
+  #cocktail.save
+  #cocktail.photo.attach(io: file, filename: 'nes.png', content_type: 'image/png')
+end
+
+puts 'Doses seeding'
+puts 'Creating doses..."-'
+cocktails = Cocktail.all
+ingredients = Ingredient.all
+cocktails.each do |cocktail|
+  7.times do
+    description = rand(1..5)
+    dose = Dose.new(description: description)
+    dose.cocktail = cocktail
+    ingredient = ingredients[rand(1..ingredients.length)]
+    dose.ingredient = ingredient
+    dose.save
+  end
+end
+
+puts 'Finished!'
